6c5fbc6261607449f0e69895cca12a1a0fbd0e02
Jean Abayo <abagammy@gmail.com>
2018-10-31 17:45:08 +0300

    Updates

---
+++ precache-manifest.f65f958a566fbc87396fa7a2d52b829e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ded75b6a86d6259efb4e43b52d5a5241",
+    "url": "/driver-license-barcode-reader/static/media/icon_barcode.ded75b6a.svg"
+  },
+  {
+    "revision": "8a59c7a62dd5e34a9839",
+    "url": "/driver-license-barcode-reader/static/js/runtime~main.8a59c7a6.js"
+  },
+  {
+    "revision": "5cb1a2d49639f2a1c957",
+    "url": "/driver-license-barcode-reader/static/js/main.5cb1a2d4.chunk.js"
+  },
+  {
+    "revision": "921e16f48158b902bc0d",
+    "url": "/driver-license-barcode-reader/static/js/1.921e16f4.chunk.js"
+  },
+  {
+    "revision": "5cb1a2d49639f2a1c957",
+    "url": "/driver-license-barcode-reader/static/css/main.06cd65bf.chunk.css"
+  },
+  {
+    "revision": "49ff9fb202969caf7addc9876c81014d",
+    "url": "/driver-license-barcode-reader/index.html"
+  }
+];
